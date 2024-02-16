f57543a9b77d6bd870d754f1c06680f4d72175b4
ludiastanly29 <160195657+ludiastanly29@users.noreply.github.com>
2024-02-16 09:37:10 +0530

    Update Person.java

--- javalearning/src/javalearning/Person.java
+++ javalearning/src/javalearning/Person.java
@@ -3,7 +3,7 @@ package javalearning;
 public class Person {
 	
 	private String name;
-	private int age =18;
+	private int age;
 	
 	
 	public Person() {
@@ -13,6 +13,7 @@ public class Person {
 	public Person(String name) {
 		super();
 		this.name = name;
+		this.age =18;
 	}
 	
 	public String getName() {
@@ -23,18 +24,18 @@ public class Person {
 		this.name = name;
 	}
 
-	public static int getAge() {
+	public int getAge() {
 		return age;
 	}
 
-	public static void setAge(int age) {
-		Person.age = age;
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public static void main(String[] args) {
 		Person person = new Person("Ludia S");
 		System.out.println("Person's Name: "+person.getName());
-		System.out.println("Person's Age: "+Person.getAge());
+		System.out.println("Person's Age: "+person.getAge());
 	}
 
 
