8cef75fb5f899fa9d1b0eb87d890c32f93c0fd1e
ludiastanly29 <160195657+ludiastanly29@users.noreply.github.com>
2024-02-16 07:17:26 +0530

    Add files via upload

---
+++ javalearning/src/javalearning/Person.java
@@ -0,0 +1,41 @@
+package javalearning;
+
+public class Person {
+	
+	private String name;
+	private static int age =18;
+	
+	
+	public Person() {
+		super();
+	}
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static int getAge() {
+		return age;
+	}
+
+	public static void setAge(int age) {
+		Person.age = age;
+	}
+
+	public static void main(String[] args) {
+		Person person = new Person("Ludia S");
+		System.out.println("Person's Name: "+person.getName());
+		System.out.println("Person's Age: "+Person.getAge());
+	}
+
+
+}
